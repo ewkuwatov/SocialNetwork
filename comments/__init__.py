00c73d9392f1873bfb6fb4cb796b5291910a184b
Your Name <you@example.com>
2023-09-09 21:08:32 +0500

    Добавлены типизацию и pydantic для comment_api

---
+++ comments/__init__.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+class CommentModel(BaseModel):
+    comment_text: str
+    user_id: int
+
+class EditCommentModel(BaseModel):
+    new_text: str
+    comment_id: int
+
--- comments/comments_api.py
+++ comments/comments_api.py
@@ -1,19 +1,21 @@
 from fastapi import APIRouter
 
+from comments import CommentModel, EditCommentModel
+
 comments_router = APIRouter(prefix='/comment', tags=['Работа с комментариями'])
 
 @comments_router.post('/add_comments')
-async def add_comments():
+async def add_comments(data: CommentModel):
     pass
 
 @comments_router.put('/edit_comments')
-async def edit_comments():
+async def edit_comments(data: EditCommentModel):
     pass
 
 @comments_router.delete('/delete_comments')
-async def delete_comments():
+async def delete_comments(comment_id: int):
     pass
 
 @comments_router.get('/get_comments')
-async def get_comments():
+async def get_comments(post_id: int):
     pass
